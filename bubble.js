6f303f110fb11f38bbf48b24f6a190ef34ad17e6
Vaishnavi Kashinath Dhumal <dhumalvaishnavi05@gmail.com>
2023-10-14 18:58:15 +0530

    Bubble Sorting

---
+++ bubble.js
@@ -0,0 +1,25 @@
+//Array sorting using Quick sort in Javascript
+//Function Declaration
+function bubbleSort(arr) {
+    const n = arr.length;
+
+    for (let i = 0; i < n - 1; i++) {
+        for (let j = 0; j < n - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap arr[j] and arr[j + 1]
+                const temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    return arr;
+}
+
+// Example usage:
+const unsortedArray = [64, 34, 25, 12, 22, 11, 90];
+const sortedArray = bubbleSort(unsortedArray);
+
+//Function Calling
+console.log(sortedArray);
